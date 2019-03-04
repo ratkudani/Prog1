f0674b3d94caf730907d12598b90a848e4b2c5a3
Dániel Ratku <43317830+ratkudani@users.noreply.github.com>
2019-03-04 21:11:05 +0100

    Add files via upload

---
+++ Feladatok/pattog2.c
@@ -0,0 +1,55 @@
+#include<stdlib.h>
+#include<time.h>
+
+
+static void gotoxy(int x, int y)           /*kurzor pozicionálása*/
+{
+  int i;
+  for(i=0; i<y; i++) printf("\n");          /*lefelé tolás*/
+  for(i=0; i<x; i++) printf(" ");           /*jobbra tolás*/
+  printf("o\n");                            /*labda ikonja*/
+
+}
+
+void usleep(int);
+int main(void)
+{
+  
+  int egyx=1;
+  int egyy=-1;
+  int i;
+  int x=10;                              /*a labda kezdeti pozíciója*/
+  int y=20;
+  int ty[23];//magasság                  /*a pálya mérete*/
+  int tx[80];//szélesség
+
+  /*pálya széleinek meghatározása*/ 
+
+  for(i=0; i<23; i++)
+       ty[i]=1;
+
+  ty[1]=-1;
+  ty[22]=-1;
+
+  for(i=0; i<79; i++)
+       tx[i]=1;
+
+  tx[1]=-1;
+  tx[79]=-1;
+
+
+
+  for(;;)
+  {
+    (void)gotoxy(x,y);
+    /*printf("o\n"); Áthelyezve a gotoxy függvényve*/
+
+    x+=egyx;
+    y+=egyy;
+
+    egyx*=tx[x];
+    egyy*=ty[y];
+
+    usleep (100000);
+    (void)system("clear");
+  }
