73c3ac3099c1f3d8e1fb8b20a342239c8d2b797d
Dániel Ratku <43317830+ratkudani@users.noreply.github.com>
2019-03-11 14:00:45 +0100

    Add files via upload

---
+++ Feladatok/nyelvszabvany.c
@@ -0,0 +1,24 @@
+//C89-ben megírt kódcsipet
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int x = 0;
+	for (x;x<10;x++){
+		printf("C89\n");
+		}
+	return 0;
+}*/
+
+//C99-ben megírt kódcsipet
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	for (int x = 0;x<10;x++){
+		printf("C99\n");
+		}
+	return 0;
+}*/
